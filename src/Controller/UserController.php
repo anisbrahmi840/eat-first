27d8312da154ac889297780fe8fb90f9938053fc
anis brahmi <anisbrahmi840@gmail.com>
2019-09-02 22:08:30 +0100

    completed admin

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -72,12 +72,13 @@ class UserController extends AbstractController
     /**
      * @Route("/{id}/edit", name="user_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, User $user): Response
+    public function edit(Request $request, User $user, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($passwordEncoder->encodePassword($user, $user->getPassword()));
             $this->getDoctrine()->getManager()->flush();
 
             if($this->isGranted(['ROLE_SUPER_ADMIN'])){
